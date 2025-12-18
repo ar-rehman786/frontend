33867b2fa716e91fd6064320a3a3389dc78d5540
info-asadghaffar <r.asadghaffar8800@gmail.com>
2025-12-18 05:07:18 +0500

    code fixed

--- app/dashboard/Reports/page.tsx
+++ app/dashboard/Reports/page.tsx
@@ -1,9 +1,14 @@
 'use client'
+import ActionBar from "@/components/dashboard/PDF-viewer/ActionBar";
+import PDFListPanel from "@/components/dashboard/PDF-viewer/PDFListPanel";
+import PDFViewer from "@/components/dashboard/PDF-viewer/PDFViewer";
 import Reports from "@/components/dashboard/Reports/Reports";
 export default function page() {
   return (
     <>
-      <Reports />
+      {/* <PDFListPanel/> */}
+      {/* <ActionBar/> */}
+      <PDFViewer/>
     </>
   )
 }
